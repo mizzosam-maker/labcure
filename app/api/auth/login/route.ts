0ec2bdbf01dee3e1755b1bf0b8e251b2966b4b8d
Samuel Hope <samuelhope805@gmail.com>
2026-05-14 14:45:50 +0300

    token fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -66,7 +66,7 @@ export async function POST(request: Request) {
     };
 
     return NextResponse.json(
-      { message: "Login successful", user: userWithoutPassword },
+      { message: "Login successful", token, user: userWithoutPassword },
       { status: 200 }
     );
   } catch (error: any) {
